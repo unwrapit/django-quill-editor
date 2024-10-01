e275124fb7972e9eb62d63843b548a919c48d868
Taylan Pince <taylanpince@gmail.com>
2024-10-01 11:52:39 +0200

    add image uploader

--- django_quill/static/django_quill/django_quill.js
+++ django_quill/static/django_quill/django_quill.js
@@ -1,25 +1,74 @@
 const djq = {}
 
 class QuillWrapper {
-    constructor(targetDivId, targetInputId, quillOptions) {
+  constructor(targetDivId, targetInputId, uploadURL, quillOptions) {
         if (!Quill.imports["modules/resize"] && quillOptions["modules"].resize) {
             Quill.register("modules/resize", window.QuillResizeModule);
         }
         if (!Quill.imports["modules/imageCompressor"] && quillOptions["modules"].imageCompressor) {
             Quill.register("modules/imageCompressor", imageCompressor);
         }
-        this.targetDiv = document.getElementById(targetDivId);
-        if (!this.targetDiv) throw 'Target div(' + targetDivId + ') id was invalid';
+    this.targetDiv = document.getElementById(targetDivId);
+    if (!this.targetDiv) throw 'Target div(' + targetDivId + ') id was invalid';
 
-        this.targetInput = document.getElementById(targetInputId);
-        if (!this.targetInput) throw 'Target Input id was invalid';
+    this.targetInput = document.getElementById(targetInputId);
+    if (!this.targetInput) throw 'Target Input id was invalid';
 
-        this.quill = new Quill('#' + targetDivId, quillOptions);
-        this.quill.on('text-change', () => {
-            var delta = JSON.stringify(this.quill.getContents());
-            var html = this.targetDiv.getElementsByClassName('ql-editor')[0].innerHTML;
-            var data = {delta: delta, html: html};
-            this.targetInput.value = JSON.stringify(data);
-        });
+    if (quillOptions.useInlineStyleAttributes) {
+      // https://quilljs.com/guides/how-to-customize-quill/#class-vs-inline
+      Quill.register(Quill.import('attributors/style/align'), true);
     }
+
+    if (uploadURL) {
+      // https://www.npmjs.com/package/quill-image-uploader
+      Quill.register("modules/imageUploader", ImageUploader);
+
+      var imageUploaderModule = {
+        upload: file => {
+          return new Promise((resolve, reject) => {
+            const formData = new FormData();
+            formData.append("image", file);
+
+            fetch(
+                uploadURL, {
+                  method: "POST",
+                  body: formData,
+                  headers: {
+                    'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+                  },
+                }
+              )
+              .then(response => response.json())
+              .then(result => {
+                console.log(result);
+                resolve(result.image_url);
+              })
+              .catch(error => {
+                reject("Upload failed");
+                alert("Uploading  failed");
+                console.error("Error:", error);
+              });
+          });
+        }
+      }
+
+    }
+
+    this.quill = new Quill('#' + targetDivId, {
+      ...quillOptions,
+      modules: {
+        ...quillOptions.modules,
+        imageUploader: imageUploaderModule
+      }
+    });
+    this.quill.on('text-change', () => {
+      var delta = JSON.stringify(this.quill.getContents());
+      var html = this.targetDiv.getElementsByClassName('ql-editor')[0].innerHTML;
+      var data = {
+        delta: delta,
+        html: html
+      };
+      this.targetInput.value = JSON.stringify(data);
+    });
+  }
 }
